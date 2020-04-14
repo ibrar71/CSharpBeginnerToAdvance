d3740ebc56a7666ffe98f5df8b2d4f6befc109fb
ibrar.se <ibrar.se@gmail.com>
2020-04-14 09:20:36 +0200

    Completed Lesson 17.

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -11,7 +11,8 @@ namespace CSharpBeginner
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            // 16. Overflowing
+
+            // 17. Something.
         }
     }
 }
